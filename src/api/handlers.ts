7f157a3bc967073fbfa33e586067937b0042ff98
skarsden <zachfrancis98@gmail.com>
2026-02-23 15:49:12 -0500

    Added: JSON validator handler; Changed: metrics and reset endpoints, reset now POST only

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -2,7 +2,13 @@ import { Response, Request } from "express";
 import { config } from "../config.js";
 
 export async function handlerMetrics(_: Request, res: Response) {
-    res.send(`Hits: ${config.fileserverHits}`);
+    res.set("Content-Type", "text/html; charset=utf-8");
+    res.send(`<html>
+    <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+    </body>
+</html>`);
 }
 
 export async function handlerReadiness(_: Request, res: Response) {
@@ -14,4 +20,24 @@ export async function handlerReset(_: Request, res: Response) {
     config.fileserverHits = 0;
     res.write("Hits reset to 0");
     res.end();
+}
+
+export async function handlerValidate(req: Request, res: Response) {
+    const params: { body: string } = req.body;
+    if (params.body.length > 140) {
+        res.header("Content-Type", "application/json");
+        res.status(400).send({ error: "Chirp is too long" });
+        return;
+    }
+
+    const words = params.body.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].toLowerCase() === "kerfuffle" || words[i].toLowerCase() === "sharbert" || words[i].toLowerCase() === "fornax") {
+            words[i] = "****";
+        }
+    }
+    const cleanedBody = words.join(" ");
+
+    res.header("Content-Type", "application/json");
+    res.status(200).send({ cleanedBody: cleanedBody });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import { middlewareLogResponses, middlewareMetricsInc } from "./middleware.js";
-import { handlerMetrics, handlerReadiness, handlerReset } from "./api/handlers.js";
+import { handlerMetrics, handlerReadiness, handlerReset, handlerValidate } from "./api/handlers.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/api/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
-app.get("/api/reset", handlerReset);
+app.post("/api/validate_chirp", handlerValidate);
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
