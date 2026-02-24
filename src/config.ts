d1ed33562445c3b27cc507e1c56a66302523fb81
skarsden <zachfrancis98@gmail.com>
2026-02-24 16:52:21 -0500

    Added: initialized postgres database, add user function

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,16 @@
+process.loadEnvFile();
+
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 }
 
-export const config: APIConfig = { fileserverHits: 0 };
+export const config: APIConfig = { fileserverHits: 0, dbURL: envOrThrow("DB_URL") };
+
+function envOrThrow(key: string) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environment variable ${key} is not set`);
+    }
+    return value;
+}
